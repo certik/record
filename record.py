c89dd89817b35fd70e6b2f0b3f51518b8b3dc73d
Ondrej Certik <ondrej@certik.cz>
2009-06-07 21:11:23 -0600

    Encode the final stuff with mencoder

--- record.py
+++ record.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 
 import sys
+import os
 from time import sleep
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, check_call
+from tempfile import mkdtemp
 
 import gst
 
@@ -11,56 +13,84 @@ from optparse import OptionParser
 class Audio(object):
 
     def __init__(self, filename):
-        self._pipeline = gst.parse_launch("""alsasrc ! audioconvert ! flacenc !  filesink location=%s""" % filename)
-        self._pipeline.set_state(gst.STATE_PLAYING)
-        print "recording to '%s'" % filename
+        self._pipe = gst.parse_launch("""alsasrc ! audioconvert ! flacenc !  filesink location=%s""" % filename)
+        self._pipe.set_state(gst.STATE_PLAYING)
 
     def stop(self):
-        self._pipeline.set_state(gst.STATE_NULL)
-        print "waiting 1s so that gstreamer finishes saving the file..."
-        sleep(1)
-        print "  done."
-
-def get_window_id():
-    p = Popen("xwininfo", stdout=PIPE)
-    out = p.communicate()[0]
-    if p.returncode != 0:
-        raise Exception("xwininfo failed")
-    s = "Window id: "
-    i1 = out.find(s) + len(s)
-    i2 = out.find(" ", i1)
-    id = out[i1: i2]
-    return id
+        self._pipe.set_state(gst.STATE_NULL)
 
 class Video(object):
 
-    def __init__(self, win_id):
+    def __init__(self, filename, win_id=None):
+        """
+        Starts capturing the video and saves it to a file 'filename'.
+
+        win_id ... the window id to capture, if None, it automatically runs
+                xwininfo and parses the output to capture the windows id
+        """
+        if win_id is None:
+            win_id = self.get_window_id()
         self._pipe = Popen(["/usr/bin/recordmydesktop", "--no-sound",
-            "-windowid", "%s" % win_id], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            "-windowid", "%s" % win_id, "-o", "%s" % filename],
+            stdin=PIPE, stdout=PIPE, stderr=PIPE)
         #p.communicate()
+
+    def __del__(self):
+        self._pipe.kill()
 
     def stop(self):
         self._pipe.terminate()
 
+    def get_window_id(self):
+        p = Popen("xwininfo", stdout=PIPE)
+        out = p.communicate()[0]
+        if p.returncode != 0:
+            raise Exception("xwininfo failed")
+        s = "Window id: "
+        i1 = out.find(s) + len(s)
+        i2 = out.find(" ", i1)
+        id = out[i1: i2]
+        return id
+
+def encode(audio, video, output):
+    """
+    Combines the audio and video to a resulting file.
+    """
+    check_call(["mencoder", "-audiofile", audio, "-oac", "lavc", "-ovc",
+        "lavc", video, "-o", output])
+
+def wait_for_ctrl_c():
+    """
+    Waits until CTRL-C is pressed.
+    """
+    try:
+        while 1:
+            pass
+    except KeyboardInterrupt:
+        pass
+
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-f", "--file", dest="filename", default="out.flac",
+    parser.add_option("-f", "--file", dest="filename", default="out.avi",
             help="save to FILE [default: %default]", metavar="FILE")
     #parser.add_option("-w", "--window", dest="window", action="store_true"
     #        help="ask which window to capture", default=True)
     options, args = parser.parse_args()
 
+    tmp_dir = mkdtemp()
+    video_file = os.path.join(tmp_dir, "video.ogv")
+    audio_file = os.path.join(tmp_dir, "audio.flac")
+    print "work dir:", tmp_dir
     print "select a window to capture"
-    win_id = get_window_id()
-    a = Audio(options.filename)
-    v = Video(win_id)
+    v = Video(video_file)
+    a = Audio(audio_file)
     print "press CTRL-C to stop"
     try:
-        try:
-            while 1:
-                pass
-        except KeyboardInterrupt:
-            pass
+        wait_for_ctrl_c()
     finally:
         v.stop()
         a.stop()
+    print "waiting 1s"
+    sleep(1)
+    encode(audio_file, video_file, options.filename)
+    print "output saved to:", options.filename
