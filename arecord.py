8e9a295a4d961d9456f7c27f972e662c3e7a5490
Ondrej Certik <ondrej@certik.cz>
2009-06-17 23:09:54 -0600

    arecord.py updated to work

--- arecord.py
+++ arecord.py
@@ -1,5 +1,26 @@
 #! /usr/bin/env python
 
-from audio import capture
+from time import sleep
+from threading import Thread
 
-capture("c.wav")
+from audio import capture, capture_stop
+
+class Audio(Thread):
+
+    def __init__(self, filename):
+        Thread.__init__(self)
+        self._filename = filename
+
+    def run(self):
+        capture(self._filename)
+
+    def stop(self):
+        capture_stop()
+
+a = Audio("c.wav")
+a.start()
+try:
+    while 1:
+        sleep(0.1)
+finally:
+    a.stop()
