c78f0e0e4ed76a7ac9a421d0b73bffc7343052fe
Ondrej Certik <ondrej@certik.cz>
2009-06-17 19:43:52 -0600

    6

--- aplay.c
+++ aplay.c
@@ -1,31 +1,3 @@
-/*
- *  aplay.c - plays and records
- *
- *      CREATIVE LABS CHANNEL-files
- *      Microsoft WAVE-files
- *      SPARC AUDIO .AU-files
- *      Raw Data
- *
- *  Copyright (c) by Jaroslav Kysela <perex@perex.cz>
- *  Based on vplay program by Michael Beck
- *
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- *
- */
-
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <malloc.h>
@@ -64,8 +36,6 @@
 #define VERSION "1.0.18"
 
 #include "formats.h"
-
-#define SND_UTIL_VERSION_STR	"1.0.18"
 
 #include <libintl.h>
 
@@ -337,7 +307,6 @@ static void pcm_list(void)
 
 static void version(void)
 {
-	printf("%s: version " SND_UTIL_VERSION_STR " by Jaroslav Kysela <perex@perex.cz>\n", command);
 }
 
 static void signal_handler(int sig)
