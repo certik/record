588e351f54c5e22afa3fb76377c76f21aa8cfacc
Ondrej Certik <ondrej@certik.cz>
2009-06-18 20:24:15 -0600

    Normalize stuff

--- amplify.py
+++ amplify.py
@@ -8,11 +8,23 @@ from sys import argv
 import wave
 import audioop
 
-def amplify(filein, fileout, factor=100):
+def normalize(filein, fileout):
     a = wave.open(filein)
     params = a.getparams()
     nchannels, sampwidth, framerate, nframes, comptype, compname = params
     data = a.readframes(nframes)
+
+    if sampwidth == 1:
+        max_val = 0x7f
+    elif sampwidth == 2:
+        max_val = 0x7fff
+    elif sampwidth == 4:
+        max_val == 0x7fffffff
+
+    max = audioop.max(data, sampwidth)
+    factor = float(max_val)/max
+    #factor = 200
+    print "amplifying by F=%f" % factor
 
     data = audioop.mul(data, sampwidth, factor)
 
@@ -21,6 +33,6 @@ def amplify(filein, fileout, factor=100):
     b.writeframes(data)
 
 if len(argv) == 3:
-    amplify(argv[1], argv[2])
+    normalize(argv[1], argv[2])
 else:
     print "usage: amplify.py infile outfile"
